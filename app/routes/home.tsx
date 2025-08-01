8c4f7629b1f70970898de1695b67baf8162f9c5a
siddharth-diwakar <siddharth.diwakar@utexas.edu>
2025-08-01 01:01:42 -0500

    Authentication

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,60 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import {resumes} from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
+import {usePuterStore} from "~/lib/puter";
+import {useLocation, useNavigate} from "react-router";
+import {useEffect} from "react";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resume Analyzer" },
+    { name: "description", content: "AI Powered Smart Resume Feedback" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+
+  const {isLoading, auth} = usePuterStore();
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    if(!auth.isAuthenticated){
+      navigate('/auth?next=/');
+    }
+  }, [auth.isAuthenticated])
+
+
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar/>
+    <section className= "main-section">
+      <div className= "page-heading py-16">
+        <h1> Track your Applications & Resume Ratings</h1>
+        <h2> Review your submissions and check AI-powered feedback</h2>
+      </div>
+
+
+
+
+    {resumes.length > 0 && (
+        <div className = "resumes-section">
+          {resumes.map((resume) => (
+              <ResumeCard key = {resume.id} resume = {resume} />
+          ))}
+        </div>
+
+
+    )}
+
+    </section>
+
+
+
+
+
+
+
+  </main>
 }
