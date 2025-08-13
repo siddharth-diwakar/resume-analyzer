e054a25db03288a56f09c3cd3966306348e1ed93
siddharth-diwakar <siddharth.diwakar@utexas.edu>
2025-08-13 00:08:29 -0500

    Data analytics & data wiping

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,4 +1,9 @@
+import {type ClassValue, clsx} from "clsx";
+import {twMerge} from "tailwind-merge";
 
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs))
+}
 
 export function formatSize(bytes: number): string {
     if (bytes === 0) return '0 Bytes';
@@ -14,5 +19,3 @@ export function formatSize(bytes: number): string {
 }
 
 export const generateUUID = () => crypto.randomUUID();
-
-
